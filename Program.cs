9251127d28063f21bae735991110725d122487fb
Rithik Goel <78385227+rithgoel@users.noreply.github.com>
2021-06-30 21:12:48 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using static System.Math;
+
+namespace project1summer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Random rnd = new Random();
+
+
+            while (true)
+            {
+                int randomNumber = rnd.Next(1, 100);
+                int counter = 1; //this allows the loop to stay true if the guess is wrong
+                bool retry = true;
+                while (true)
+                {
+
+                    Console.WriteLine("Enter guess: ");
+                    int guess = Convert.ToInt32(Console.ReadLine());
+
+                    if (guess < randomNumber)
+                    {
+                        Console.WriteLine("Too low; try again.");
+                        ++counter;
+                        continue;
+
+                    }
+                    else if (guess > randomNumber)
+                    {
+                        Console.WriteLine("Too High, try again.");
+                        ++counter;
+                        continue;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Good job. You guess the number");
+                        Console.WriteLine("Would you like to retey? y/n");
+                        string entry = Console.ReadLine();
+                        if (entry != "y")
+                        {
+                            retry = false;
+                        }
+                        break;
+
+
+                    }
+
+                }
+                if (!retry) break;
+            }
+        }
+    }
+}
